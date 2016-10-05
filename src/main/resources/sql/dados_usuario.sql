4846241d8da9f51e88b75be877f9eb398675a06c
Matheus Herculano <Matheus Herculano@192.168.25.20>
2016-10-05 17:30:05 -0300

    mudança de sql de dados basicos

--- src/main/resources/sql/dados_usuario.sql
+++ src/main/resources/sql/dados_usuario.sql
@@ -1,24 +1,19 @@
 INSERT INTO sasac.perfil (perfil) 
-	VALUES ('Coordenador');
-INSERT INTO sasac.perfil (perfil) 
-	VALUES ('Aluno');
+	VALUES ('Administrador');
 INSERT INTO sasac.perfil (perfil) 
 	VALUES ('Professor');
-INSERT INTO sasac.repeticao (repeticao) 
-	VALUES ('Semanal');
-INSERT INTO sasac.repeticao (repeticao) 
-	VALUES ('Mensal');
-INSERT INTO sasac.repeticao (repeticao) 
-	VALUES ('Bimestral');
-INSERT INTO sasac.repeticao (repeticao) 
-	VALUES ('Trimestral');
-INSERT INTO sasac.repeticao (repeticao) 
-	VALUES ('Semestral');
-INSERT INTO sasac.repeticao (repeticao) 
-	VALUES ('Anual');
+INSERT INTO sasac.perfil (perfil) 
+	VALUES ('Aluno');
 INSERT INTO sasac.usuario (nome_usuario, perfil_id) 
 	VALUES ('Fernando', 1);
 INSERT INTO sasac.usuario (nome_usuario, perfil_id) 
-	VALUES ('Matheus', 2);
+	VALUES ('Sandra', 2);
 INSERT INTO sasac.usuario (nome_usuario, perfil_id) 
-	VALUES ('Rafael', 3);
+	VALUES ('Rafael', 2);
+INSERT INTO sasac.usuario (nome_usuario, perfil_id) 
+	VALUES ('Matheus', 3);
+INSERT INTO sasac.usuario (nome_usuario, perfil_id) 
+	VALUES ('Patricia', 3);
+INSERT INTO sasac.usuario (nome_usuario, perfil_id) 
+	VALUES ('Hugo', 3);
+
